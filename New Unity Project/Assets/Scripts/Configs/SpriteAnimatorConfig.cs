e1e7ff73d69fa0e2450c88000183048c2d609631
Blavez <sergeitroitsckij@yandex.ru>
2021-07-25 18:37:43 +0300

    First lesson

---
+++ New Unity Project/Assets/Scripts/Configs/SpriteAnimatorConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+
+    public enum AnimState 
+    {
+        Idle = 0,
+        Run=1,
+        Jump=2
+    }
+    [CreateAssetMenu(fileName = "SpriteAnimationCfg", menuName = "Configs/ Animation", order = 1)]
+    public class SpriteAnimatorConfig : ScriptableObject
+    {
+        [Serializable]
+        public sealed class SpriteSequance
+        {
+            public AnimState Track;
+            public List<Sprite> Sprites = new List<Sprite>();
+        }
+        public List<SpriteSequance> Sequence = new List<SpriteSequance>();
+    }
+}
