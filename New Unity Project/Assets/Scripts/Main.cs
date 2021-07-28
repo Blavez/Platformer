42344825bd80242dddaef4cb0e730f5cea68039c
Blavez <sergeitroitsckij@yandex.ru>
2021-07-28 18:04:12 +0300

    second lesson

--- New Unity Project/Assets/Scripts/Main.cs
+++ New Unity Project/Assets/Scripts/Main.cs
@@ -8,16 +8,19 @@ namespace PlatformerMVC
         [SerializeField] private int _animationSpeed=10;
         [SerializeField] private LevelObjectView _playerView;
         private SpriteAnimatorController _playerAnimator;
+        private PlayerController _playerController;
         void Awake()
         {
             _playerConfig = Resources.Load<SpriteAnimatorConfig>("PlayerAnimCfg");
             _playerAnimator = new SpriteAnimatorController(_playerConfig);
-            _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
+            
+
+            _playerController = new PlayerController(_playerView, _playerAnimator);
         }
 
         void Update()
         {
-            _playerAnimator.Update();
+            _playerController.Update();
         }
 
         private void FixedUpdate()
